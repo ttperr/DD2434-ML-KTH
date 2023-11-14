f1a37c87027419607a71267f6109eaa890c55124
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2023-11-14 17:18:12 +0100

    Update CAVI.py and main.pdf with new functions and corrected formulas

--- Assignment 1/1A/py_files/cavi.py
+++ Assignment 1/1A/py_files/cavi.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.special as sp_spec
+import scipy.stats as sp_stats
 import numpy.random as np_rand
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
@@ -33,3 +34,21 @@ plt.savefig('12_data.png')
 
 
 # Question 1.3.14
+
+def update_tau(x, mu_prime, lambda_prime, a_0, b_0, mu_0, lambda_0):
+    E_mu = mu_prime
+    E_mu2 = 1 / lambda_prime + mu_prime ** 2
+
+    a_prime = a_0 + x.shape[0] / 2
+    b_prime = b_0 + 0.5 * (np.sum(x ** 2) - 2 * np.sum(x) * E_mu +
+                           x.shape[0] * E_mu2 + lambda_0*(E_mu2 + mu_0 ** 2 - 2 * E_mu * mu_0))
+
+    return a_prime, b_prime
+
+
+def update_mu(x, a_prime, b_prime, mu_0, lambda_0):
+    E_tau = a_prime / b_prime
+    mu_prime = (lambda_0 * mu_0 + np.sum(x)) / (lambda_0 + x.shape[0])
+    lambda_prime = (lambda_0 + x.shape[0])*E_tau
+
+    return mu_prime, lambda_prime
