5830006fae8640cbf12e802a51685b8ff9b5f2fd
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2023-11-14 10:37:26 +0100

    Answering question 3.12

---
+++ Assignment 1/1A/py_files/12.py
@@ -0,0 +1,31 @@
+from typing import List
+import numpy as np
+import scipy.special as sp_spec
+import numpy.random as np_rand
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import adjusted_rand_score
+import seaborn as sns
+
+
+def generate_data(N: int, mu: float, tau: float) -> np.ndarray:
+    return np_rand.normal(mu, tau, N)
+
+
+def plot_data(X: np.ndarray, ax: plt.Axes) -> None:
+    ax.hist(X, bins=20, density=True)
+    ax.set_xlabel('x')
+    ax.set_ylabel('p(x)')
+    ax.set_title(f'N = {len(X)}')
+
+
+MU = 1
+TAU = 0.5
+N = [10, 100, 1000]
+Xs = [generate_data(n, MU, TAU) for n in N]
+
+fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+for i in range(len(Xs)):
+    plot_data(Xs[i], axs[i])
+plt.tight_layout()
+plt.savefig('12_data.png')
