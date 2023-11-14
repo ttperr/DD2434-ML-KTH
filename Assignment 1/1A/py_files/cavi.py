ab4002afa603d44021deaed71ed40f3c32db66a8
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2023-11-14 17:00:45 +0100

    Refactor code and update documentation

--- Assignment 1/1A/py_files/cavi.py
+++ Assignment 1/1A/py_files/cavi.py
@@ -1,4 +1,3 @@
-from typing import List
 import numpy as np
 import scipy.special as sp_spec
 import numpy.random as np_rand
@@ -7,6 +6,8 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import adjusted_rand_score
 import seaborn as sns
 
+
+# Question 1.3.12
 
 def generate_data(N: int, mu: float, tau: float) -> np.ndarray:
     return np_rand.normal(mu, tau, N)
@@ -29,3 +30,6 @@ for i in range(len(Xs)):
     plot_data(Xs[i], axs[i])
 plt.tight_layout()
 plt.savefig('12_data.png')
+
+
+# Question 1.3.14
