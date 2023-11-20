Fix variance calculation in ML_est function and update data generation in 3.12.py

Assignment 1/1A/py_files/3.12.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import gamma, norm
 from scipy.special import psi
@@ -5,18 +6,18 @@ np.random.seed(14)
 
 
 def generate_data(mu, tau, N):
-  # Insert your code here
-  D = np.random.normal(mu, 1/tau, N)
+    # Insert your code here
+    D = np.random.normal(mu, np.sqrt(1/tau), N)
 
-  return D
+    return D
 
 
-mu = 1
-tau = 0.5
+MU = 1
+TAU = 0.5
 
-dataset_1 = generate_data(mu, tau, 10)
-dataset_2 = generate_data(mu, tau, 100)
-dataset_3 = generate_data(mu, tau, 1000)
+dataset_1 = generate_data(MU, TAU, 10)
+dataset_2 = generate_data(MU, TAU, 100)
+dataset_3 = generate_data(MU, TAU, 1000)
 
 # Visulaize the datasets via histograms
 # Insert your code here

Assignment 1/1A/py_files/3.13.py
@@ -2,7 +2,7 @@ def ML_est(data):
     # insert your code
     N = len(data)
     x_mean = np.mean(data)
-    x_var = np.var(data, ddof=1)
+    x_var = np.var(data)
 
     tau_ml = 1 / x_var
     mu_ml = x_mean

Assignment 1/1A/py_files/3.14.py
@@ -6,8 +6,8 @@ def compute_exact_posterior(D, a_0, b_0, mu_0, lambda_0):
     mu_prime = (lambda_0 * mu_0 + N * x_mean) / (lambda_0 + N)
     lambda_prime = lambda_0 + N
     a_prime = a_0 + N / 2
-    b_prime = b_0 + 1 / 2 * (np.sum((D - x_mean)**2) +
-                             lambda_0 * mu_0**2 - lambda_prime * mu_prime**2)
+    b_prime = b_0 + 0.5 * (np.sum(D**2) +
+                           lambda_0 * mu_0**2 - lambda_prime * mu_prime**2)
 
     exact_post_distribution = (a_prime, b_prime, mu_prime, lambda_prime)
 
@@ -20,8 +20,8 @@ lambda_0 = 10
 a_0 = 20
 b_0 = 20
 
-mus = np.linspace(0.3, 1.2, 200)
-taus = np.linspace(0.1, 1, 200)
+mus = np.linspace(-0.25, 1.1, 200)
+taus = np.linspace(0.4, 0.9, 200)
 
 fig, axs = plt.subplots(1, 3, figsize=(12, 4))
 for i, dataset in enumerate([dataset_1, dataset_2, dataset_3]):
@@ -36,12 +36,12 @@ for i, dataset in enumerate([dataset_1, dataset_2, dataset_3]):
         pMu = norm(loc=mu_T, scale=1/np.sqrt(lambda_T*tau))
         Z_exact[:, j] = pMu.pdf(mus) * pTau.pdf(tau)
     # Finding the maximum of the exact posterior
-    mu_max = mus[np.argmax(np.max(Z_exact, axis=1))]
-    tau_max = taus[np.argmax(np.max(Z_exact, axis=0))]
+    mu_max_exact = mus[np.argmax(np.max(Z_exact, axis=1))]
+    tau_max_exact = taus[np.argmax(np.max(Z_exact, axis=0))]
     # Plotting the results
     axs[i].contour(*np.meshgrid(mus, taus), Z_exact.T,
                    levels=5, colors=['green'])
-    axs[i].plot(mu_max, tau_max, 'ro', label='MAP')
+    axs[i].plot(mu_max_exact, tau_max_exact, 'ro', label='MAP')
     axs[i].plot(mu_ml, tau_ml, 'b+', label='ML Estimate')
     axs[i].plot(MU, TAU, 'gx', label='Actual')
     axs[i].legend()

Assignment 1/1A/py_files/3.15.py
@@ -1,53 +1,35 @@
-eps = 1e-6
-
-
 def compute_elbo(D, a_0, b_0, mu_0, lambda_0, a_N, b_N, mu_N, lambda_N):
     N = len(D)
     x_mean = np.mean(D)
     x_2_sum = np.sum(D**2)
 
-    # Expected log likelihood
-    expected_log_likelihood = 0.5 * N * (np.log(lambda_N) - np.log(
-        2 * np.pi)) - 0.5 * lambda_N * (x_2_sum - 2 * x_mean * mu_N * N + N * mu_N**2)
+    elbo = 0  # to delete
+    # compute the elbo
 
-    # Expected log prior for mu
-    expected_log_prior_mu = 0.5 * (np.log(lambda_0) - np.log(2 * np.pi)) - \
-        0.5 * lambda_0 * (mu_N**2 - 2 * mu_N * mu_0 + mu_0**2)
-
-    # Expected log prior for tau
-    expected_log_prior_tau = a_0 * np.log(b_0) - np.log(gamma(a_0).pdf(0)) + (
-        a_0 - 1) * (psi(a_N) - np.log(b_N)) - b_0 * (a_N / b_N)
-
-    # Entropy of variational distribution for mu
-    entropy_mu = 0.5 * (1 + np.log(2 * np.pi)) + 0.5 * np.log(1 / lambda_N)
-
-    # Entropy of variational distribution for tau
-    entropy_tau = a_N - np.log(b_N) + \
-        np.log(gamma(a_N).pdf(0) + eps) + (1 - a_N) * psi(a_N)
-
-    # Compute ELBO
-    elbo = expected_log_likelihood + expected_log_prior_mu + \
-        expected_log_prior_tau + entropy_mu + entropy_tau
+    # Entropy of mu
+    entropy_mu = norm.entropy(loc=mu_N, scale=1/np.sqrt(lambda_N))
+    # Entropy of tau
+    entropy_tau = gamma.entropy(a=a_N, scale=1/b_N)
 
     return elbo
 
 
-def CAVI(D, a_0, b_0, mu_0, lambda_0, iter=10):
+def CAVI(D, a_0, b_0, mu_0, lambda_0, iter=5):
     # make an initial guess for the expected value of tau
-    initial_guess_exp_tau = 1
+    E_tau = 1
 
     N = len(D)
     x_mean = np.mean(D)
     x_2_sum = np.sum(D**2)
 
     # Constants
-    a_N = a_0 + N / 2
+    a_N = a_0 + (N+1) / 2
     mu_N = (lambda_0 * mu_0 + N * x_mean) / (lambda_0 + N)
     E_mu = mu_N
 
-    # Variational parameters
-    b_N = b_0
-    lambda_N = lambda_0
+    # Variables
+    b_Ns = []
+    lambda_Ns = []
 
     # ELBO
     elbos = []
@@ -55,75 +37,97 @@ def CAVI(D, a_0, b_0, mu_0, lambda_0, iter=10):
     # CAVI iterations ...
     for i in range(iter):
         # update the values for the variational parameters
-        E_tau = a_N / b_N
-        E_mu_2 = 1 / lambda_N + mu_N**2
-
         lambda_N = (lambda_0 + N) * E_tau
-        b_N = b_0 + 1 / 2 * (x_2_sum + N*E_mu_2 - 2*N*E_mu *
-                             x_mean + lambda_0*(E_mu_2 - 2*E_mu*mu_0 + mu_0**2))
+
+        E_mu_2 = 1 / lambda_N + mu_N**2
+        b_N = b_0 + 0.5 * (x_2_sum + N*E_mu_2 - 2*N*E_mu*x_mean +
+                           lambda_0*(E_mu_2 - 2*E_mu*mu_0 + mu_0**2))
+
+        E_tau = a_N / b_N
+
+        b_Ns.append(b_N)
+        lambda_Ns.append(lambda_N)
         # save ELBO for each iteration, plot them afterwards to show convergence
         elbos.append(compute_elbo(D, a_0, b_0, mu_0,
                      lambda_0, a_N, b_N, mu_N, lambda_N))
 
-    return a_N, b_N, mu_N, lambda_N, elbos
+    return a_N, b_N, mu_N, lambda_N, elbos, b_Ns, lambda_Ns
 
 
-# Insert your main code here
-mu_ml, tau_ml = ML_est(dataset_2)
-a_N, b_N, mu_N, lambda_N, elbos = CAVI(dataset_2, a_0, b_0, mu_0, lambda_0)
-exact_post_dist = compute_exact_posterior(dataset_2, a_0, b_0, mu_0, lambda_0)
+def compute_z_exact(mus, taus, a_, b_, mu_, lambda_):
+    z = np.zeros((len(mus), len(taus)))
+    pTau = gamma(a=a_, loc=0, scale=1/b_)
+    for j, tau in enumerate(taus):
+        pMu = norm(loc=mu_, scale=1/np.sqrt(lambda_*tau))
+        z[:, j] = pMu.pdf(mus) * pTau.pdf(tau)
+
+    return z
 
 
-mu_vals = np.linspace(-1, 1, 100)
-tau_vals = np.linspace(0, 2, 100)
-mu_grid, tau_grid = np.meshgrid(mu_vals, tau_vals)
-
-# Calculate the Z values for contours
-
-
-def calculate_density(mu, tau, mu_0, lambda_0, a_0, b_0):
-    mu_prior = norm(mu_0, 1/np.sqrt(lambda_0)).pdf(mu)
-    tau_prior = gamma(a_0, scale=1/b_0).pdf(tau)
-    return mu_prior * tau_prior
+def compute_z_cavi(mus, taus, a_, b_, mu_, lambda_):
+    z = np.zeros((len(mus), len(taus)))
+    pTau = gamma(a=a_, loc=0, scale=1/b_)
+    pMu = norm(loc=mu_, scale=1/np.sqrt(lambda_))
+    z = np.outer(pMu.pdf(mus), pTau.pdf(taus))
+    return z
 
 
-Z = np.zeros_like(mu_grid)
-for i in range(mu_grid.shape[0]):
-    for j in range(mu_grid.shape[1]):
-        Z[i, j] = calculate_density(
-            mu_grid[i, j], tau_grid[i, j], mu_N, lambda_N, a_N, b_N)
+iter = 3  # number of iterations for CAVI
+mus = np.linspace(-0.2, 1.1, 200)
+taus = np.linspace(0.1, 1.1, 200)
 
-# Plot the contour
-plt.figure(figsize=(10, 6))
-contour = plt.contour(mu_grid, tau_grid, Z, colors='k')
-plt.clabel(contour, inline=1, fontsize=10)
-plt.xlabel('mu')
-plt.ylabel('tau')
-plt.title('Contour plot for the variational posterior')
+xlims = [[-0.2, 1.1], [0.3, 1.1], [0.7, 1.1]]
+ylims = [[0.4, 1.1], [0.4, 0.7], [0.4, 0.55]]
+
+elbos_list = []
+
+fig, axs = plt.subplots(iter, 3, figsize=(30, 30))
+for i, dataset in enumerate([dataset_1, dataset_2, dataset_3]):
+    mu_ml, tau_ml = ML_est(dataset)
+    a_N, b_N, mu_N, lambda_N, elbos, b_Ns, lambda_Ns = CAVI(
+        dataset, a_0, b_0, mu_0, lambda_0, iter=iter)
+    a_T, b_T, mu_T, lambda_T = compute_exact_posterior(
+        dataset, a_0, b_0, mu_0, lambda_0)
+
+    elbos_list.append(elbos)
+
+    for j in range(iter):
+        Z_exact = compute_z_exact(mus, taus, a_T, b_T, mu_T, lambda_T)
+        Z_cavi = compute_z_cavi(mus, taus, a_N, b_Ns[j], mu_N, lambda_Ns[j])
+        # Finding the maximum of the exact posterior
+        mu_max_exact = mus[np.argmax(np.max(Z_exact, axis=1))]
+        tau_max_exact = taus[np.argmax(np.max(Z_exact, axis=0))]
+        # Finding the maximum of the CAVI approximation
+        mu_max_cavi = mus[np.argmax(np.max(Z_cavi, axis=1))]
+        tau_max_cavi = taus[np.argmax(np.max(Z_cavi, axis=0))]
+        # Plotting the results
+        axs[j, i].contour(*np.meshgrid(mus, taus), Z_exact.T,
+                          levels=5, colors=['green'])
+        axs[j, i].contour(*np.meshgrid(mus, taus), Z_cavi.T,
+                          levels=5, colors=['red'])
+        axs[j, i].plot(mu_max_exact, tau_max_exact, 'r+', label='MAP (Exact)')
+        axs[j, i].plot(mu_max_cavi, tau_max_cavi, 'mx', label='MAP (CAVI)')
+        axs[j, i].plot(mu_ml, tau_ml, 'bo', label='ML Estimate')
+        axs[j, i].plot(MU, TAU, 'go', label='Actual')
+        axs[j, i].legend()
+        axs[j, i].grid()
+        axs[j, i].set_xlabel('mu')
+        axs[j, i].set_ylabel('tau')
+        axs[j, i].set_title(f'Dataset {i+1}, iteration {j}')
+        axs[j, i].set_xlim(xlims[i])
+        axs[j, i].set_ylim(ylims[i])
+plt.tight_layout()
+plt.savefig('15_contours.png')
 plt.show()
-
-
-print('mu_ml: ', mu_ml)
-print('tau_ml: ', tau_ml)
-print('a_N: ', a_N)
-print('b_N: ', b_N)
-print('mu_N: ', mu_N)
-print('lambda_N: ', lambda_N)
-print('expected tau: ', a_N / b_N)
-print('exact_post_dist (a, b, mu, lambda): ', exact_post_dist)
-print('exact expected tau: ', exact_post_dist[0] / exact_post_dist[1])
 
 # Plot ELBOs
-plt.plot(elbos)
-plt.xlabel('Iterations')
-plt.ylabel('ELBO')
-# plt.savefig('elbo.png')
+fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+for i in range(3):
+    axs[i].plot(elbos[i])
+    axs[i].set_xlabel('Iteration')
+    axs[i].set_title(f'Dataset {i+1}')
+    axs[i].grid()
+plt.tight_layout()
+plt.title('ELBO')
+plt.savefig('15_elbo.png')
 plt.show()
-
-# Example flow for dataset_2:
-
-# mu_ml, tau_ml = ML_est(dataset_2)
-# a_N, b_N, mu_N, lambda_N, elbos = CAVI(dataset_2, a_0, b_0, mu_0, lambda_0)
-# plot elbos, show convergence
-# exact_post_dist = compute_exact_posterior(dataset_2, a_0, b_0, mu_0, lambda_0)
-# compare exact_post_dist with the CAVI result ( = q(a_N, b_N, mu_N, lambda_N) ) using for ex. contour plots, show also ML estimate on this plot