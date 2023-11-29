57aa7f00a878ea2e616d521cb9c2f0dd0d549075
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2023-11-30 00:40:15 +0100

    Adding files

---
+++ Project/py_files/3.12.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.stats import gamma, norm
+from scipy.special import psi
+from scipy.special import gamma as gamma_func
+np.random.seed(14)
+
+
+def generate_data(mu, tau, N):
+    # Insert your code here
+    D = np.random.normal(mu, np.sqrt(1/tau), N)
+
+    return D
+
+
+MU = 1
+TAU = 0.5
+
+dataset_1 = generate_data(MU, TAU, 10)
+dataset_2 = generate_data(MU, TAU, 100)
+dataset_3 = generate_data(MU, TAU, 1000)
+
+# Visulaize the datasets via histograms
+# Insert your code here
+fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+axs[0].hist(dataset_1, bins=20)
+axs[1].hist(dataset_2, bins=20)
+axs[2].hist(dataset_3, bins=20)
+plt.tight_layout()
+plt.savefig('../images/12_data.png')
+plt.show()
