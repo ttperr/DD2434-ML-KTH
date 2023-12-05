21b99f8a34348428c66fc825dd1dc6e3cc973cb8
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2023-12-05 16:59:26 +0100

    Remove unnecessary code files

--- Assignment 1/1B/py_files/1.2-gen.py
+++
@@ -1,7 +0,0 @@
-with torch.no_grad():
-    # insert your code here to create images from noise (it is enough to create theta value for each pixel)
-    #
-    #
-    # generated_images = ....  # should be a matrix ( batch_size-by-x_dim )
-    generated_images = torch.round(
-        model.Decoder(torch.randn(batch_size, latent_dim)))
--- Assignment 1/1B/py_files/1.2-reparam.py
+++
@@ -1,7 +0,0 @@
-def reparameterization(self, mean, var):
-    # insert your code here
-    std = torch.sqrt(var + 1e-10)
-    eps = torch.randn_like(std)
-    z = mean + std * eps
-
-    return z
